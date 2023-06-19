57167e032b983a69313fc3dab89b608c37e525d6
samdiobar <sbp5602@gmail.com>
2023-06-19 15:20:15 -0500

    Hello World frontend render

--- app.js
+++ app.js
@@ -1,22 +1,24 @@
-var express = require('express');
+// required external modules
+const express = require('express');
+const path = require("path");
 
-//init app object
+// app variables
 var app = express();
 const port = 3000;
+const home_page_router = require('./routes/home-page');
 
-//routers
-var home_page_router = require('./routes/home-page');
-
-app.use('/', home_page_router);
-
-//static files
+// static files
 app.use('/public', express.static('public'));
 
-//The 404 Route
+// routes
+app.use('/', home_page_router);
+
+// error 404 Route
 app.get('*', function(req, res){
   res.status(404).send('error 404: page not found');
 });
 
+// start app
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
--- routes/home-page.js
+++ routes/home-page.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('Hello World!')
+  res.sendFile(path.join(__dirname, '/pages/home-page.html'));
 });
 
 router.get('/hey', function(req, res, next) {
